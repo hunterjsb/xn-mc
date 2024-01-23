0541db820da3f13b4ab0cc90b2cec39864cb0fd8
hunter <hboyd@supersod.com>
2024-01-23 16:39:44 -0500

    working backup to s3. god i hope the word size stays below 5gb

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import zipfile
 from typing import AnyStr
@@ -7,20 +8,18 @@ import boto3
 from utils.config import Config
 
 
-def _s3():
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('xanmc')
-    for obj in bucket.objects.all():
-        print(obj.key)
+def upload_file_to_s3(zip_fp: str, bucket: str, object_name: str):
+    s3_client = boto3.client('s3')
+    return s3_client.upload_file(zip_fp, bucket, object_name)
 
 
 def zip_world(world_fp: AnyStr, output_file_name: str):
     # Determine the parent directory
     parent_dir = os.path.dirname(world_fp)
-    output_filename = os.path.join(parent_dir, output_file_name + '.zip')
+    output_fp = os.path.join(parent_dir, output_file_name + '.zip')
 
     # Create a ZipFile object in write mode
-    with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    with zipfile.ZipFile(output_fp, 'w', zipfile.ZIP_DEFLATED) as zipf:
         # Walk through the directory
         for root, dirs, files in os.walk(world_fp):
             for file in files:
@@ -30,7 +29,7 @@ def zip_world(world_fp: AnyStr, output_file_name: str):
                 # Add the file to the zip file
                 zipf.write(file_path, file_path_in_zip)
 
-    return output_filename
+    return output_fp
 
 
 if __name__ == '__main__':
@@ -38,3 +37,6 @@ if __name__ == '__main__':
 
     zipped_to = zip_world(cfg.world_filepath, cfg.world_name)
     print(f'Zipped world to {zipped_to}')
+
+    resp = upload_file_to_s3(zipped_to, cfg['S3_BUCKET'], cfg.world_name + '.zip')
+    print(f'Sent zip to AWS, responded with: {resp}')
