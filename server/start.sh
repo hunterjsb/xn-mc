92932dc6baec839f34089d890b302121972fdc23
Ubuntu <hunter@mc-server-eastus.mfciphsuvv0u5miefr4l1atxvc.bx.internal.cloudapp.net>
2024-01-24 04:21:55 +0000

    start.sh simplify

--- server/start.sh
+++ server/start.sh
@@ -4,5 +4,4 @@
 tmux new-session -d -s minecraft
 
 # Send the Java command to the tmux session and redirect output to a file
-tmux send-keys -t minecraft "java -Xms1024M -Xmx7G -jar server.jar nogui &> ~/xn-mc/minecraft_server_output.log & echo \$! > ~/xn-mc/minecraft_server.pid" C-m
-
+nohup java -Xms1024M -Xmx7G -jar server.jar nogui &> server.out &
